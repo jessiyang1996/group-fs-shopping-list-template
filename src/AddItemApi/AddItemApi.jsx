3b0f13e6a739b2aa348378ac8a7ef869b348513a
Ying Her <yher.prime@gmail.com>
2024-01-27 11:49:28 -0600

    commiting deleting button working

--- src/AddItemApi/AddItemApi.jsx
+++ src/AddItemApi/AddItemApi.jsx
@@ -1,22 +1,22 @@
 // imports
-import axios from "axios";
+import axios from 'axios';
 
 // get Axios call
 export const fetchItems = () => {
-    return axios.get('api/shoppingList');
+  return axios.get('api/shoppingList');
 };
 
 // axios POST call
 export const postItems = (itemData) => {
-    return axios.post(`api/shoppingList`, itemData);
+  return axios.post(`api/shoppingList`, itemData);
 };
 
 // axios PUT (Update) call
 export const updateItemPurchased = (itemId) => {
-    return axios.put(`/api/hoppingList.${itemId}`);
+  return axios.put(`/api/shoppingList.${itemId}`);
 };
 
 // axios Delete call
 export const deleteItems = (itemId) => {
-    return axios.delete(`/api/shoppingList/${itemId}`);
+  return axios.delete(`/api/shoppingList/${itemId}`);
 };
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,78 @@
 import { useState, useEffect } from 'react';
 import Header from '../Header/Header.jsx';
 import './App.css';
-import { fetchItems } from '../../AddItemApi/AddItemApi.jsx';
 import Form from '../Form/Form.jsx';
+import { fetchItems, deleteItems } from '../../AddItemApi/AddItemApi.jsx';
 
 function App() {
-    // declare our variables
-    const [shoppingList, setShoppingList] = useState([
-        {name: "milk", quantity: 1, unit: "gallon"}
-    ]); // end of useState (declaring our variables)
+  // declare our variables
+  const [shoppingList, setShoppingList] = useState([
+    { name: 'milk', quantity: 1, unit: 'gallon' },
+  ]); // end of useState (declaring our variables)
 
-    // Refresh Shopping List
-    const refreshShoppingList = () => {
-        const listPromise = fetchItems();
-        listPromise
-            .then((response) => {
-                // success; Axios GET request & promise
-                console.log("Data from server: ", response);
-                setShoppingList(response.data);
-            })
-            .catch((error) => {
-                // not successful
-                console.error('Error in Axios GET: ', error);
-            });
-    }; // end of Refresh Shopping List
+  // Refresh Shopping List
+  const refreshShoppingList = () => {
+    const listPromise = fetchItems();
+    listPromise
+      .then((response) => {
+        // success; Axios GET request & promise
+        console.log('Data from server: ', response);
+        setShoppingList(response.data);
+      })
+      .catch((error) => {
+        // not successful
+        console.error('Error in Axios GET: ', error);
+      });
+  }; // end of Refresh Shopping List
 
-    // initial load of components
-    useEffect(() => {
-        console.log('Testing Items');
-        // api call
+  // initial load of components
+  useEffect(() => {
+    console.log('Testing Items');
+    // api call
+    refreshShoppingList();
+  }, []); // end of useEffect - initial load of components
+
+  const handleDeleteButton = (id) => {
+    //ID item
+    console.log('DELETE - itemId:', id);
+    //MAKE axios call
+    deleteItems(id)
+      .then((response) => {
         refreshShoppingList();
-    }, []); // end of useEffect - initial load of components
-    
-    // Everything in the return will appear on the DOM
-    return (
-        <div className="App">
-            <Header />
-            <Form refreshShoppingList={refreshShoppingList}/> 
-            <main>
-                {/* render shopping list onto page: .map loops our data for us */}
-                <h1>Shopping List</h1>
-                    {shoppingList.map((itemData, dataIndex) => {
-                        return (
-                            <div key={dataIndex}>
-                                {/* How we want the data to appear on the DOM: */}
-                                <h3>{itemData.name}</h3>
-                                <p>{itemData.quantity} {itemData.unit}</p>
-                                <button type="button">Purchased</button>
-                                <button type="button">Delete</button>
-                            </div>
-                        )
-                    })}
-            </main>
-        </div>
-    );
+      })
+      .catch((error) => {
+        console.error('ERROR in deleting items', error);
+      });
+  };
+  // Everything in the return will appear on the DOM
+  return (
+    <div className="App">
+      <Header />
+      <Form refreshShoppingList={refreshShoppingList} />
+      <main>
+        {/* render shopping list onto page: .map loops our data for us */}
+        <h1>Shopping List</h1>
+        {shoppingList.map((itemData, dataIndex) => {
+          return (
+            <div key={dataIndex}>
+              {/* How we want the data to appear on the DOM: */}
+              <h3>{itemData.name}</h3>
+              <p>
+                {itemData.quantity} {itemData.unit}
+              </p>
+              <button type="button">Purchased</button>
+              <button
+                onClick={(event) => handleDeleteButton(itemData.id)}
+                type="button"
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </main>
+    </div>
+  );
 }
 
 export default App;
